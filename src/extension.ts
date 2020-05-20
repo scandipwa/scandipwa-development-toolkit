ac1154cfc4997709851273f50df14973c712737a
yeegor <yegor.batov@gmail.com>
2020-05-20 21:44:32 +0300

    Add extend commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,14 @@ import * as vscode from 'vscode';
 
 import { createQuery, extendQuery } from './commands/query';
 import createComponent from './commands/create-component';
+import extendComponent, { Extendable } from './commands/extend-component';
 import { validateScandiPWA } from './util/file';
 
 const commandMap = {
 	'scandipwa-devtools.createNewComponent': createComponent.bind(null, false),
 	'scandipwa-devtools.createNewRoute': createComponent.bind(null, true),
+	'scandipwa-devtools.extendCoreComponent': extendComponent.bind(null, Extendable.component),
+	'scandipwa-devtools.extendCoreRoute': extendComponent.bind(null, Extendable.route),
 	'scandipwa-devtools.createNewQuery': createQuery,
 	'scandipwa-devtools.extendCoreQuery': extendQuery
 };
