328d7ee2bd91b13a675d4a849c3fa57ee297430d
yeegor <yegor.batov@gmail.com>
2020-06-08 00:20:26 +0300

    Relevantt validator error messages

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,16 +1,16 @@
-const validateComponentName = (value: string | undefined) => {
+const validateName = (value: string | undefined) => {
     if (!value) {
-        return 'Component name must not be empty.';
+        return 'Name must not be empty.';
     }
     if (/\s/.test(value)) {
-        return 'Component name can not contain spaces.';
+        return 'Name cannot contain spaces.';
     }
     if (value[0] !== value[0].toUpperCase()) {
-        return 'Component name must start from upper-case letter.';
+        return 'Name must start from upper-case letter.';
     }
     return null;
 };
 
 module.exports = {
-    validateComponentName
+    validateName
 };
